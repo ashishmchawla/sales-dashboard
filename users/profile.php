800c0252352d853b1879474ab2133fc062b3a872
Ashish Chawla <ashishmchawla@gmail.com>
2023-01-27 00:15:16 +0530

    get user details

---
+++ users/profile.php
@@ -0,0 +1,36 @@
+<?php
+
+session_start();
+
+if( !isset($_SESSION['token']) ) {
+	header("Location: ../");
+} else {
+	?>
+<?php include '../includes/header.php' ?>
+<title>Users Profile | Triventure Sales </title>
+<?php include '../includes/sidebar.php'; ?>
+<div id="main" class="p-3">
+    <div class="containerBox">
+        <h2 id="profile_name"></h2>
+    </div>
+</div>
+
+<script>
+var id = '<?php echo $_GET['id']; ?>';
+$(document).ready(function() {
+    $.ajax({
+        url: baseUrl + '/user_details/' + id,
+        type: 'get',
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(errorData) {
+
+        }
+    });
+});
+</script>
+<?php
+	include '../includes/footer.php';
+}
+?>
