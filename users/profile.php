845e04236e6ceebe79482e7cdde033758f6c2120
Ashish Chawla <ashish.chawla@sugarboxnetworks.com>
2023-01-27 16:19:05 +0530

    Adding Targets for Individual Users

--- users/profile.php
+++ users/profile.php
@@ -12,42 +12,141 @@ if( !isset($_SESSION['token']) ) {
 <div id="main" class="p-3">
     <div class="containerBox">
         <h2 id="profile_name"></h2>
-        <hr/>
+        <hr />
         <ul class="nav nav-pills" id="myTab" role="tablist">
             <li class="nav-item">
-                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="home" aria-selected="true">Profile</a>
+                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#profile" role="tab"
+                    aria-controls="home" aria-selected="true">Profile</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" id="pan-tab" data-toggle="tab" href="#targets" role="tab" aria-controls="pan" aria-selected="false">Targets</a>
+                <a class="nav-link" id="pan-tab" data-toggle="tab" href="#targets" role="tab" aria-controls="pan"
+                    aria-selected="false">Targets</a>
             </li>
         </ul>
-    <br/>
-    <div class="tab-content profile-tab" id="myTabContent">
-        <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">                <h3>Profile Details</h3>
-            <br/>
-            <div class="row">
-                <div class="col">
-                    <h6 class="title">First Name</h6>
-                    <h4 class="title_value" id="first_name"></h4>
+        <br />
+        <div class="tab-content profile-tab" id="myTabContent">
+            <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                <h3>Profile Details</h3>
+                <br />
+                <div class="row">
+                    <div class="col">
+                        <h6 class="title">First Name</h6>
+                        <h4 class="title_value" id="first_name"></h4>
+                    </div>
+                    <div class="col">
+                        <h6 class="title">Last Name</h6>
+                        <h4 class="title_value" id="last_name"></h4>
+                    </div>
                 </div>
-                <div class="col">
-                    <h6 class="title">Last Name</h6>
-                    <h4 class="title_value" id="last_name"></h4>
+                <div class="row">
+                    <div class="col">
+                        <h6 class="title">Phone</h6>
+                        <h4 class="title_value" id="phone"></h4>
+                    </div>
+                    <div class="col">
+                        <h6 class="title">Email</h6>
+                        <h4 class="title_value_smallcase" id="email"></h4>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col">
-                    <h6 class="title">Phone</h6>
-                    <h4 class="title_value" id="phone"></h4>
+            <div class="tab-pane fade show" id="targets" role="tabpanel" aria-labelledby="targets-tab">
+                <h3>Target Details</h3>
+                <div class="d-flex flex-row-reverse bd-highlight">
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-link" data-toggle="modal" data-target="#targetModal">
+                        Edit Targets
+                    </button>
                 </div>
-                <div class="col">
-                    <h6 class="title">Email</h6>
-                    <h4 class="title_value_smallcase" id="email"></h4>
-                </div>
+                <br />
+                <table class="display table hover" width="100%" id="targetTable">
+                    <thead>
+                        <tr>
+                            <th>Month - Year</th>
+                            <th>Type</th>
+                            <th>Achieved</th>
+                            <th>Target</th>
+                        </tr>
+                    </thead>
+                </table>
             </div>
         </div>
-        <div class="tab-pane fade show" id="targets" role="tabpanel" aria-labelledby="targets-tab">
-                <h3>Target Details</h3>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="targetModal" tabindex="-1" aria-labelledby="targetModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="targetModalLabel">Edit Target</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form id="updateTarget">
+                        <div class="form-group">
+                            <label for="monthyear">Month Year</label>
+                            <p> <?php echo date('M - Y'); ?> </p>
+                            <input type="hidden" name="user_id" value="<?php echo $_GET['id']; ?>">
+                            <input type="hidden" name="month" value="<?php echo date('n'); ?>">
+                            <input type="hidden" name="year" value="<?php echo date('Y'); ?>">
+                        </div>
+                        <h6>Targets</h6>
+                        <hr />
+                        <div class="form-group row">
+                            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">New
+                                Leads:</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" name="new">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Existing
+                                Leads:</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" name="existing">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="colFormLabelSm"
+                                class="col-sm-2 col-form-label col-form-label-sm">Account:</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" name="account">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="colFormLabelSm"
+                                class="col-sm-2 col-form-label col-form-label-sm">Margin:</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" name="margin">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Mutual
+                                Funds:</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" name="mutual_funds">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="colFormLabelSm"
+                                class="col-sm-2 col-form-label col-form-label-sm">Insurance:</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" name="insurance">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Third
+                                party:</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" name="third_party">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" id="save_targets" class="btn btn-primary">Save changes</button>
+                </div>
             </div>
         </div>
     </div>
@@ -63,10 +162,9 @@ $(document).ready(function() {
             'Authorization': "Bearer " + token
         },
         success: function(data) {
-            if( data.status === 1 ) {
+            if (data.status === 1) {
                 let userDetails = data.details;
-                console.log(userDetails.first_name);
-                $('#profile_name').html(userDetails.first_name+' '+userDetails.last_name);
+                $('#profile_name').html(userDetails.first_name + ' ' + userDetails.last_name);
                 $('#first_name').html(userDetails.first_name);
                 $('#last_name').html(userDetails.last_name);
                 $('#email').html(userDetails.email);
@@ -75,6 +173,37 @@ $(document).ready(function() {
         },
         error: function(errorData) {}
     });
+
+    var table = $('#targetTable').DataTable({
+        "scrollX": true,
+        "ajax": {
+            url: baseUrl + "/user_targets_table/" + id,
+            headers: {
+                'Authorization': "Bearer " + token
+            },
+        },
+    });
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+        $($.fn.dataTable.tables(true)).DataTable()
+            .columns.adjust();
+    });
+
+    $('#save_targets').on('click', () => {
+        let inputData = $("#updateTarget").serialize();
+        console.log(inputData);
+        $.ajax({
+            url: baseUrl + '/set_user_targets',
+            type: 'post',
+            headers: {
+                'Authorization': "Bearer " + token
+            },
+            data: inputData,
+        }).done(function(response) {
+            console.log('Successful hit!');
+            console.log(response);
+        })
+    });
+
 });
 </script>
 <?php
