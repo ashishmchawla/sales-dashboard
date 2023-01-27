deec2c9d4fa68a5c59051fd541d45289f809c152
Ashish Chawla <ashish.chawla@sugarboxnetworks.com>
2023-01-27 17:01:56 +0530

    Changes for Lead Details

--- leads/profile.php
+++ leads/profile.php
@@ -12,78 +12,103 @@ if( !isset($_SESSION['token']) ) {
 <div id="main" class="p-3">
     <div class="containerBox">
         <h2 id="profile_name"></h2>
-        <hr/>
+        <hr />
         <ul class="nav nav-pills" id="myTab" role="tablist">
             <li class="nav-item">
-                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="home" aria-selected="true">Profile</a>
+                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#profile" role="tab"
+                    aria-controls="home" aria-selected="true">Profile</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" id="pan-tab" data-toggle="tab" href="#targets" role="tab" aria-controls="pan" aria-selected="false">Targets</a>
+                <a class="nav-link" id="pan-tab" data-toggle="tab" href="#activities" role="tab" aria-controls="pan"
+                    aria-selected="false">Activites</a>
             </li>
         </ul>
-    <br/>
-    <div class="tab-content profile-tab" id="myTabContent">
-        <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">                <h3>Profile Details</h3>
-            <br/>
-            <div class="row">
-                <div class="col">
-                    <h6 class="title">First Name</h6>
-                    <h4 class="title_value" id="first_name"></h4>
+        <br />
+        <div class="tab-content profile-tab" id="myTabContent">
+            <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                <h3>Profile Details</h3>
+                <hr />
+                <br />
+                <div class="row">
+                    <div class="col">
+                        <h6 class="title">First Name</h6>
+                        <hr />
+                        <h4 class="title_value" id="first_name"></h4>
+                    </div>
+                    <div class="col">
+                        <h6 class="title">Last Name</h6>
+                        <hr />
+                        <h4 class="title_value" id="last_name"></h4>
+                    </div>
                 </div>
-                <div class="col">
-                    <h6 class="title">Last Name</h6>
-                    <h4 class="title_value" id="last_name"></h4>
+                <br />
+                <div class="row">
+                    <div class="col">
+                        <h6 class="title">Phone</h6>
+                        <hr />
+                        <h4 class="title_value" id="phone"></h4>
+                    </div>
+                    <div class="col">
+                        <h6 class="title">Email</h6>
+                        <hr />
+                        <h4 class="title_value" id="email"></h4>
+                    </div>
+                </div>
+                <br />
+                <div class="row">
+                    <div class="col">
+                        <h6 class="title">Location</h6>
+                        <hr />
+                        <h4 class="title_value" id="location"></h4>
+                    </div>
+                    <div class="col">
+                        <h6 class="title">Lead Status</h6>
+                        <hr />
+                        <h4 class="title_value"><span class="badge badge-primary" id="lead_status"></span></h4>
+                    </div>
+                </div>
+                <br />
+                <div class="row">
+                    <div class="col">
+                        <h6 class="title">Account Category</h6>
+                        <hr />
+                        <h4 class="title_value" id="account_category"></h4>
+                    </div>
+                    <div class="col">
+                        <h6 class="title">Account Code</h6>
+                        <hr />
+                        <h4 class="title_value" id="account_code"></h4>
+                    </div>
+                </div>
+                <br />
+                <div class="row">
+                    <div class="col">
+                        <h6 class="title">Third Party</h6>
+                        <hr />
+                        <h4 class="title_value" id="third_party"></h4>
+                    </div>
+                    <div class="col">
+                        <h6 class="title">Stock Margin</h6>
+                        <hr />
+                        <h4 class="title_value" id="stock_margin"></h4>
+                    </div>
+                </div>
+                <br />
+                <div class="row">
+                    <div class="col">
+                        <h6 class="title">Lead Owner</h6>
+                        <hr />
+                        <h4 class="title_value" id="lead_owner"></h4>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col">
-                    <h6 class="title">Phone</h6>
-                    <h4 class="title_value" id="phone"></h4>
+            <div class="tab-pane fade show" id="activities" role="tabpanel" aria-labelledby="targets-tab">
+                <h3>Activities</h3>
+                <hr />
+                <div id="cardContainer" class="cardContainer"></div>
+                <div id="blankDiv">
+                    <h3>No activities available for this lead</h3>
                 </div>
-                <div class="col">
-                    <h6 class="title">Email</h6>
-                    <h4 class="title_value" id="email"></h4>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <h6 class="title">Location</h6>
-                    <h4 class="title_value" id="location"></h4>
-                </div>
-                <div class="col">
-                    <h6 class="title">Lead Status</h6>
-                    <h4 class="title_value_badge" id="lead_status"></h4>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <h6 class="title">Account Category</h6>
-                    <h4 class="title_value" id="account_category"></h4>
-                </div>
-                <div class="col">
-                    <h6 class="title">Account Code</h6>
-                    <h4 class="title_value" id="account_code"></h4>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <h6 class="title">Third Party</h6>
-                    <h4 class="title_value" id="third_party"></h4>
-                </div>
-                <div class="col">
-                    <h6 class="title">Stock Margin</h6>
-                    <h4 class="title_value" id="stock_margin"></h4>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <h6 class="title">Lead Owner</h6>
-                    <h4 class="title_value" id="lead_owner"></h4>
-                </div>
-            </div>
-        </div>
-        <div class="tab-pane fade show" id="targets" role="tabpanel" aria-labelledby="targets-tab">
-                <h3>Target Details</h3>
             </div>
         </div>
     </div>
@@ -93,28 +118,56 @@ if( !isset($_SESSION['token']) ) {
 let id = '<?php echo $_GET['id']; ?>';
 $(document).ready(function() {
     $.ajax({
-        url: baseUrl + '/user_details/' + id,
+        url: baseUrl + '/lead_details/' + id,
         type: 'get',
         headers: {
             'Authorization': "Bearer " + token
         },
         success: function(data) {
-            if( data.status === 1 ) {
-                let userDetails = data.details;
-                console.log(userDetails.first_name);
-                $('#profile_name').html(userDetails.first_name+' '+userDetails.last_name);
-                $('#first_name').html(userDetails.first_name);
-                $('#last_name').html(userDetails.last_name);
-                $('#email').html(userDetails.email);
-                $('#contact').html(userDetails.contact);
-                $('#location').html(userDetails.location);
-                $('#account_category').html(userDetails.account_category);
-                $('#account_code').html(userDetails.account_code);
-                $('#lead_status').html(userDetails.lead_status);
-                $('#lead_owner').html(userDetails.lead_owner);
+            if (data.status === 1) {
+                let leadDetails = data.details;
+                console.log(leadDetails);
+                $('#profile_name').html(leadDetails.first_name + ' ' + leadDetails.last_name);
+                $('#first_name').html(leadDetails.first_name);
+                $('#last_name').html(leadDetails.last_name);
+                $('#email').html(leadDetails.email);
+                $('#contact').html(leadDetails.contact);
+                $('#location').html(leadDetails.location);
+                $('#account_category').html(leadDetails.account_category);
+                $('#account_code').html(leadDetails.account_code);
+                $('#lead_status').html(leadDetails.lead_status);
+                $('#lead_owner').html(leadDetails.owner_first_name + ' ' + leadDetails
+                    .owner_last_name);
             }
         },
         error: function(errorData) {}
+    });
+    $.ajax({
+        url: baseUrl + '/lead_activities/' + id,
+        type: 'get',
+        headers: {
+            'Authorization': "Bearer " + token
+        },
+        success: function(data) {
+            if (data.status === 0) {
+                $('#cardContainer').css('display', 'none');
+                $('#blankDiv').css('display', 'block');
+            } else {
+                let activities = data.details;
+                console.log(activities);
+                $('#cardContainer').css('display', 'block');
+                $('#blankDiv').css('display', 'none');
+                for (var i = 0; i < activities.length; i++) {
+                    $('#cardContainer').append('<div class="card"><div class="card-header">' +
+                        activities[i].activity_type +
+                        '</div><div class="card-body"><h5 class="card-title">' + activities[i]
+                        .created_at +
+                        '</h5><p class="card-text">' + activities[i].activity_log +
+                        '</p> <a href = "#" class = "btn btn-primary"> Go somewhere </a></div></div>'
+                    );
+                }
+            }
+        }
     });
 });
 </script>
